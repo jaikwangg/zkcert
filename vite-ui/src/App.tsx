b84452c629aeeb6af1580ed9e54adf102f0ed61d
jaikwangg <kanna9764@gmail.com>
2025-06-26 18:56:42 +0700

    fix: set basename for GitHub Pages routing

--- vite-ui/src/App.tsx
+++ vite-ui/src/App.tsx
@@ -18,7 +18,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/zkcert">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/jobs" element={<Jobs />} />
