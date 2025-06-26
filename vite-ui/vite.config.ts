e0b27b3fa4b51d7038b1ba72eaabb9912143937d
jaikwangg <kanna9764@gmail.com>
2025-06-26 18:16:35 +0700

    fix path error

--- vite-ui/vite.config.ts
+++ vite-ui/vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // เปลี่ยนชื่อ repo ตามจริง!
 export default defineConfig({
   plugins: [react()],
   base: '/zkcert/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'), // <--- ใส่ alias ให้ตรง
+    },
+  },
 })
