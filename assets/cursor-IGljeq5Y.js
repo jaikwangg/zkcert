fab672710cf71c80949e6554401486c6a0e70eb1
jaikwangg <jaikwangg@users.noreply.github.com>
2025-06-26 12:45:59 +0000

    deploy: 944a6719bda6545e1fec5d6dc02f644499b2f4dc

---
+++ assets/cursor-IGljeq5Y.js
@@ -0,0 +1,3 @@
+import{m as o}from"./core-COWmGLAX.js";import"./index-CmJD86om.js";import"./index.es-Bm5RU99i.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
+  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
+</svg>`;export{l as cursorSvg};
